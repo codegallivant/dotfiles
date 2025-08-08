70f717ca18bf44c029b11b6df2faf8b82f5c8651
codegallivant <janak3.1415et@gmail.com>
2025-08-08 17:05:38 +0530

    update rofi-playerctl

--- private_dot_config/polybar/common/scripts/rofi-playerctl/executable_rofi-playerctl.sh
+++ private_dot_config/polybar/common/scripts/rofi-playerctl/executable_rofi-playerctl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+status=$(playerctl status 2>/dev/null || echo "exception")
+if [[ "$status" != "Playing" && "$status" != "Paused" ]]; then
+  exit
+fi
 rofi_command="rofi -theme $HOME/.config/rofi/applets/type-1/style-2.rasi -dmenu $*"
 
 output=$(playerctl metadata --format '{{title}} - {{artist}}')
